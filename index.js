cabc8eb774340e013fa6720f7dc473311a4b3ebb
Timothy Jensen <timothy.jensen.13@gmail.com>
2023-01-13 15:32:32 -0500

    commit tree f9e3ab8bf4d3b4ad7b1f0dd19416597518d185b1

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+
+//const gifts = ["teddy bear", "drone", "doll"];
+//
+//function wrapGifts(gifts) {
+//    for (let i = 0; i < gifts.length; i++) {
+//        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+//
+//   return gifts;
+//}
+//
+//wrapGifts(gifts);
+
+const messages = [];
+const names = [];
+let event = "birthday"
+
+function writeCards(names, event) {
+    for (let c = 0; c < names.length; c++) {
+       messages.push(`Thank you, ${names[c]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+writeCards(names, "birthday");
+
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+
+}
